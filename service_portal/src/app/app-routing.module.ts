1a26175de9bb14d6637e1101bb1535bafb20f53f
Gokula krishnan <gokulakrishnanbca20@gmail.com>
2022-01-12 07:55:14 +0530

    routing 1

--- service_portal/src/app/app-routing.module.ts
+++ service_portal/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { StudentLoginComponent } from './login/student-login/student-login.component';
+import { TeacherLoginComponent } from './login/teacher-login/teacher-login.component';
+import { StudentComponent } from './student/student.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'',component:LoginComponent},
+  {path:'/login/student',component:StudentLoginComponent},
+  {path:'/login/teacher',component:TeacherLoginComponent},
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
