f9163a82a0536a08fcb92431b9c17d44de963ddc
SanjayBaskaran <sanjayprakashsivakasi@gmail.com>
2022-01-18 23:04:36 -0500

    Heroku F

--- server.js
+++ server.js
@@ -5,11 +5,8 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/service-portal'));
+app.use(express.static(__dirname+'./dist/service-portal'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/service-portal/'}),
-);
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
