59c91f575aa6629efa6ed261dbb98f5e22dd7ff3
ziaulakram <mziaulakram@gmail.com>
2022-01-24 18:54:00 +0530

    Student structure

--- service_portal/src/app/student/bonafide/bonafide.component.ts
+++ service_portal/src/app/student/bonafide/bonafide.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { StudentDataService } from './../../student-data.service';
 @Component({
   selector: 'app-bonafide',
   templateUrl: './bonafide.component.html',
   styleUrls: ['./bonafide.component.scss']
 })
 export class BonafideComponent implements OnInit {
-
-  constructor() { }
+  studentbio:any;
+  constructor(private student:StudentDataService) { }
 
   ngOnInit(): void {
+    this.studentbio = this.student.getStudentBio();
+    //console.log(this.studentbio);
   }
-
 }
