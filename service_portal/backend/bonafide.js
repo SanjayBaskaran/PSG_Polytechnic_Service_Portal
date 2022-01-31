f30ef961d1036d669f51d8644b1da8d764b0f052
SanjayBaskaran <sanjayprakashsivakasi@gmail.com>
2022-01-31 15:37:13 -0500

    Added auth middleware for responses and request route in server

--- service_portal/backend/bonafide.js
+++ service_portal/backend/bonafide.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const con = require("./database");
+const jwt = require("jsonwebtoken");
 var router = express.Router();
 
-router.post("/request", (req, res) => {
+router.post("/request",(req,res,next)=>{
+  try{
+    let token = req.headers.authentication;
+    var data = jwt.verify(token,"SECRET_CODE_USER_LOGIN");
+    req.userData = data.rno;
+    next();
+  }catch(error){
+    console.log("Error");
+    res.status(401).json({"Auth":false});
+  }
+}, (req, res) => {
   let curr = new Date();
 
   let query = "INSERT INTO bonafide values('"+req.body.rno+"','"+req.body.batch_id+"','"+req.body.bonafide_type+"','"+req.body.bonafide_reason+"','NNN'"+",STR_TO_DATE('"+"" +curr.getDate() +"/"+ curr.getMonth()+1 + "/"+curr.getFullYear()+"','%d/%m/%Y'),NULL,'"+req.body.comment+"');";
@@ -13,13 +24,21 @@ router.post("/request", (req, res) => {
     res.json({"Record":"Inserted"});
   });
 });
-router.post("/responses",(req,res)=>{
-  let query="SELECT * FROM bonafide WHERE rno='"+req.body.rno+"';";
+router.post("/responses",(req,res,next)=>{
+  try{
+    let token = req.headers.authentication;
+    var data = jwt.verify(token,"SECRET_CODE_USER_LOGIN");
+    req.userData = data.rno;
+    next();
+  }catch(error){
+    console.log("Error");
+    res.status(401).json({"Auth":false});
+  }
+},(req,res)=>{
+  let query="SELECT * FROM bonafide WHERE rno='"+req.userData+"';";
   con.query(query,function(err,result,fields){
     if (err) throw err;
-    console.log(query);
     if (result.length > 0){
-      console.log(result);
       res.json(result);
     }
   })
