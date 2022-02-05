83de57591249c6286c4d3cf9f9d75dc45254c755
unknown <kuttiesdharani@gmail.com>
2022-02-05 17:45:34 +0530

    admin-student

--- service_portal/src/app/admin/stud/stud.component.ts
+++ service_portal/src/app/admin/stud/stud.component.ts
@@ -10,22 +10,22 @@ import { DomSanitizer } from '@angular/platform-browser';
 })
 export class StudComponent implements OnInit {
   studentInfo:any;
-  image:any;
+  image:Array<any>=[];
   constructor(private admindata:AdminService,private router:Router, private domsanitizer:DomSanitizer) { 
-    this.studentInfo = {"rno": "", "stud_name": "", "gender": "", "dob": "", "stud_address": "", "stud_pass": "", "photo":new Blob(),"stud_mobile":0,"stud_email":"","father_name":"","mother_name":"","year_of_joining":0,"year_of_passing":0,"batch_id":"","dept_id":"","programme_name":""};
   }
 
   ngOnInit(): void {
     this.admindata.student().subscribe(
       (data:any)=>{
         this.studentInfo=data;
-        let TYPED_ARRAY = new Uint8Array(data.photo.data);
+        for(let i=0;i<this.studentInfo.length;i++){
+          let TYPED_ARRAY = new Uint8Array(data[i].photo.data);
         const STRING_CHAR = TYPED_ARRAY.reduce((data, byte)=> {
           return data + String.fromCharCode(byte);
           }, '');
           let base64String = btoa(STRING_CHAR);
-          this.image = this.domsanitizer.bypassSecurityTrustUrl("data:image/jpg;base64, " + base64String);
-
+          this.image[i] = this.domsanitizer.bypassSecurityTrustUrl("data:image/jpg;base64, " + base64String);
+        }
         console.log(data);
       },(err)=>{
         this.router.navigate(['adminLogin']);
