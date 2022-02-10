6a89d1ef989c547f8e4e08afe351769fda088397
SanjayBaskaran <sanjayprakashsivakasi@gmail.com>
2022-02-09 23:25:06 -0500

    Admin Staffs

--- service_portal/src/app/admin/staff/staff.component.ts
+++ service_portal/src/app/admin/staff/staff.component.ts
@@ -1,3 +1,4 @@
+import { DomSanitizer } from '@angular/platform-browser';
 import { AdminService } from './../../admin.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
@@ -9,15 +10,25 @@ import { Router } from '@angular/router';
 })
 export class StaffComponent implements OnInit {
   teacherInfo:any;
+  staff:any;
 
-
-  constructor(private admindata:AdminService,private router:Router) { }
+  constructor(private admindata:AdminService,private router:Router,private domsanitizer:DomSanitizer) { }
 
   ngOnInit(): void {
     this.admindata.teacher().subscribe(
       (data)=>{
         this.teacherInfo=data;
-        console.log(data);
+        this.staff = {};
+        this.teacherInfo.forEach((element:any) => {
+          if (!this.staff[element.dept_id]) {
+            this.staff[element.dept_id] = [];
+          }
+          this.staff[element.dept_id].push(element);
+        });
+        this.teacherInfo = Object.keys(this.staff).map((key,index)=>{
+          return {"dept":key,"data":this.staff[key]};
+        });
+        console.log(this.teacherInfo);
       },(err)=>{
         this.router.navigate(['adminLogin']);
       }
